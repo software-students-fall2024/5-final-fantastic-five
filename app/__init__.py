d65f156b40164d29851d129d3e1d87f6eb53a978
alexandramastrangelo <arm9129@nyu.edu>
2024-11-21 18:12:28 -0500

    added mongo uri

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
+mongo_uri = os.getenv("MONGO_URI") 
+client = MongoClient(mongo_uri)
+db = client["wishlist_db"]
+
+@app.route("/")
 def home():
-    return "Environment setup complete!"
+    return "Connected to MongoDB Atlas!"
 
 if __name__ == "__main__":
     app.run(debug=True)
